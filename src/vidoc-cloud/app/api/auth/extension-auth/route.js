fa8b1255d1febaa5cae3953b4431a3e90905eaeb
Henry Müssemann <hm@freezingdata.de>
2023-10-18 17:47:49 +0200

    Documentation

--- src/vidoc-cloud/app/api/auth/extension-auth/route.js
+++ src/vidoc-cloud/app/api/auth/extension-auth/route.js
@@ -1,7 +1,7 @@
 // api/custom-login.js
 import { handleLogin, handleAuth } from "@auth0/nextjs-auth0";
 
-
+/* :vidoc 9e519762-8997-45e2-814c-cdcc20731962.mp4 */
 export const GET = handleLogin({
   authorizationParams: {
     redirect_uri: "http://localhost:3000/api/auth/extension-auth/callback",
